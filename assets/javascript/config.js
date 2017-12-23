e8f29fa5f3ba89284d829b4760141f956c346948
Peian Wang <mangopeian@gmail.com>
2017-12-23 01:57:52 -0800

    finish project

---
+++ assets/javascript/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCI2WnqmnfkFTPdBvvbTGejerzXRWnDf3o",
+	authDomain: "rockpaperscissors123456.firebaseapp.com",
+	databaseURL: "https://rockpaperscissors123456.firebaseio.com",
+	storageBucket: "rockpaperscissors123456.appspot.com",
+	messagingSenderId: "366125598976"
+	};
+firebase.initializeApp(config);
